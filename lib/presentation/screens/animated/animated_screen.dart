85fdf9221f049af832c4bf93b8b4449e5c5a9b33
Daniel Alejandro Gimenez <danielalejandro347@gmail.com>
2024-11-12 15:55:44 -0300

    Animated container finished.

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,12 +1,65 @@
+import 'dart:math' show Random, min;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 50; // It generates a width between 50 and 350
+    height =
+        random.nextInt(300) + 50; // It generates a height between 50 and 350
+    borderRadius = random.nextInt(60) + 10;
+
+    color = Color.fromRGBO(
+      random.nextInt(255), // red
+      random.nextInt(255), // green
+      random.nextInt(255), // blue
+      1, // opacity
+    );
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeInOut, // elasticInOut
+          width: width,
+          height: height,
+          constraints: const BoxConstraints(
+            minWidth: 50,
+            minHeight: 50,
+          ),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
