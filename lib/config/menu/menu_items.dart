ede53592633e703770d4e742d94ecb00695b79ea
Daniel Alejandro Gimenez <danielalejandro347@gmail.com>
2024-11-12 20:10:46 -0300

    The tutorial screen menu option has been added

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -45,4 +45,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Una serie de controles de Flutter',
       link: '/ui-controls',
       icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introducción a la aplicación',
+      subTitle: 'Pequeño tutorial introductorio',
+      link: '/tutorial',
+      icon: Icons.accessible_rounded),
 ];
